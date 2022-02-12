8431d7d6ea6b80d67fa8d382e33eb15a64a11700
Seamus McGill <seamusmcgill@gmail.com>
2022-02-12 15:26:06 -0500

    Add seeds for organizations and categories

---
+++ db/seeds/01_organizations.sql
@@ -0,0 +1,4 @@
+INSERT INTO organizations
+  (name, password, organization_url, description, logo_url)
+VALUES
+  ('Lighthouse Labs', 'password', 'https://www.lighthouselabs.ca/en', 'Lighthouse Labs was created in 2013 by a team of software developers with a passion for code, mentorship, and education. Our mission: to continuously find the best ways to train the next generation of developers and transform the way tech education is delivered.', 'https://www.lighthouselabs.ca/assets/lighthouselabs-logo-d5df6d383f7466b06a64de9561fd537493502eafe78bbf3bcd3de82884e5dd85.png');
---
+++ db/seeds/03_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories
+  (creator_id, name, description)
+VALUES
+  (1, 'Social', 'Social media networks and apps.'),
+  (1, 'Productivity', 'Organizational tools.'),
+  (2, 'Retail', 'For making online purchases.'),
+  (3, 'Licensing', 'For software licensing.'),
+  (1, 'Internet', 'ISP details.');
