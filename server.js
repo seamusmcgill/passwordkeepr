05654a880d197088f96da8368531cc28d10289c2
Andrew McNeill <andrew@andrewmc.ca>
2022-02-12 16:37:59 -0400

    GET / redirects to HTML in new destination

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use("/api/categories", categoriesRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("./public/index");
 });
 
 app.listen(PORT, () => {
