d796a8b2cc01a8808433762ee41a167d25ae7b8f
Andrew McNeill <andrew@andrewmc.ca>
2022-02-12 19:56:04 -0400

    More project structuring

--- README_passwordkeepr.md
+++ README_passwordkeepr.md
@@ -48,5 +48,22 @@
 ├── styles
 │   ├── layout.css
 │   └── main.css
-└── server.js
+└── server
+    ├── database.js
+    └── server.js
+
 ```
+
+* `/public` contains all of the HTML, CSS, and client side JavaScript. 
+  * `/public/index.html` is the entry point to the application. It's the only html page because this is a single page application.
+  * `/public/scripts` contains all of the client side javascript files.
+    * `app.js` starts up the application by rendering the listings.
+    * `network.js` manages all ajax requests to the server.
+    * `views_manager.js` manages which components appear on screen.
+    * `components` contains all of the individual html components. They are all created using jQuery.
+  * `/public/styles` contains all of the SASS-generated CSS files.
+* `styles` contains all of the sass files. 
+* `server` contains all of the server side and database code.
+  * `server.js` is the entry point to the application. This connects the routes to the database.
+  * `apiRoutes.js` and `userRoutes.js` are responsible for any HTTP requests to `/users/something` or `/api/something`. 
+  * `database.js` is responsible for all queries to the database.
--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 // Web server config
 const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
+const sassMiddleware = require("../lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
+const dbParams = require("../lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
@@ -35,10 +35,10 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const passwordsRoutes = require("./routes/passwords");
-const organizationsRoutes = require("./routes/organizations");
-const categoriesRoutes = require("./routes/categories");
+const usersRoutes = require("../routes/users");
+const passwordsRoutes = require("../routes/passwords");
+const organizationsRoutes = require("../routes/organizations");
+const categoriesRoutes = require("../routes/categories");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
