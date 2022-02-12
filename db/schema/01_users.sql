e29978188bd891eac2970ddd181cc7ebe848b115
Andrew McNeill <andrew@andrewmc.ca>
2022-02-12 14:32:06 -0400

    Minor fixes to schema syntax and data types

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  organization_id NOT NULL REFERENCES organizations(id),
+  organization_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
   full_name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
   password VARCHAR(255) NOT NULL
--- db/schema/03_categories.sql
+++ db/schema/03_categories.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS categories CASCADE;
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
-  creator_id REFERENCES users(id),
+  creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   name VARCHAR(255),
   description TEXT,
 );
