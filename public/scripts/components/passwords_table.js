a325b76df2223c40b335717b8d1dbdf2c39d3efe
Andrew McNeill <andrew@andrewmc.ca>
2022-02-12 21:38:13 -0400

    jQuery to render passwords table

---
+++ public/scripts/components/passwords_table.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  const tableHTML = `
+  <table>
+  <tr>
+  <th>Name</th>
+  <th>URL</th>
+  <th>Username</th>
+  <th>Password</th>
+  </tr>
+  </table>
+  `;
+
+  $('body').append(tableHTML);
+
+  const getPasswords = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/api/passwords',
+    }).then((response) => {
+      renderPasswords(response);
+    });
+  };
+
+  const renderPasswords = passwords => {
+    const passwordsArray = passwords.passwords;
+    for (let password of passwordsArray) {
+      $('table').append(createPasswordElement(password));
+    }
+  };
+
+  const createPasswordElement = password => {
+    const passwordHTML = `
+    <tr>
+      <td>${password.service_name}</td>
+      <td>${password.service_url} </td>
+      <td>${password.login_username}</td>
+      <td>${password.login_password}</td>
+    </tr>
+    `;
+    return passwordHTML;
+  };
+
+  getPasswords();
+
+});
