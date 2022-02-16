3d76b39036fa53a24c1ade1f92d4250d870f78d8
Seamus McGill <seamusmcgill@gmail.com>
2022-02-16 16:00:45 -0500

    Add all header logic

---
+++ public/scripts/components/header.js
@@ -0,0 +1,87 @@
+$(() => {
+  window.header = {};
+
+  const $navBar = $('#navbar');
+  let currentUser = null;
+  const updateHeader = (user) => {
+    currentUser = user;
+    console.log(user);
+    $navBar.empty();
+
+    let userLinks;
+
+    if (!user) {
+      userLinks = `
+      <a id="homepage" href="#">PasswordKeepR</a>
+      <div>
+        <a id="registerLink" href="#">Register</a>
+      </div>
+      `;
+    } else {
+      userLinks = `
+      <a id="homepage" href="#">PasswordKeepR</a>
+      <div>
+        <a id="newCategoryLink" href="#">+Category</a>
+        <a id="newPasswordLink" href="#">+Password</a>
+        <p>Logged in as ${user.name}, ${user.organization}</p>
+      </div>
+      `;
+    }
+
+    $navBar.append(userLinks);
+  };
+
+  window.header.update = updateHeader;
+
+  getCurrentUser()
+    .then(( json ) => {
+      updateHeader(json.user);
+    });
+
+  // $("header").on("click", '.my_reservations_button', function() {
+  //   propertyListings.clearListings();
+  //   getAllReservations()
+  //     .then(function(json) {
+  //       propertyListings.addProperties(json.reservations, true);
+  //       views_manager.show('listings');
+  //     })
+  //     .catch(error => console.error(error));
+  // });
+  // $("header").on("click", '.my_listing_button', function() {
+  //   propertyListings.clearListings();
+  //   getAllListings(`owner_id=${currentUser.id}`)
+  //     .then(function(json) {
+  //       propertyListings.addProperties(json.properties);
+  //       views_manager.show('listings');
+  //   });
+  // });
+
+  // $("header").on("click", '.home', function() {
+  //   propertyListings.clearListings();
+  //   getAllListings()
+  //     .then(function(json) {
+  //       propertyListings.addProperties(json.properties);
+  //       views_manager.show('listings');
+  //   });
+  // });
+
+  // $('header').on('click', '.search_button', function() {
+  //   views_manager.show('searchProperty');
+  // });
+
+  // $("header").on('click', '.login_button', () => {
+  //   views_manager.show('logIn');
+  // });
+  // $("header").on('click', '.sign-up_button', () => {
+  //   views_manager.show('signUp');
+  // });
+  // $("header").on('click', '.logout_button', () => {
+  //   logOut().then(() => {
+  //     header.update(null);
+  //   });
+  // });
+
+  // $('header').on('click', '.create_listing_button', function() {
+  //   views_manager.show('newProperty');
+});
+
