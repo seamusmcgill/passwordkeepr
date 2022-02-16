54e7cf896f745994d03883da0d84b474de978b54
Seamus McGill <seamusmcgill@gmail.com>
2022-02-16 10:26:17 -0500

    Move generatePassword function

--- public/scripts/components/new_password_form.js
+++ public/scripts/components/new_password_form.js
@@ -27,70 +27,7 @@ $(document).ready(function() {
     $('section').append(newPasswordHTML);
   });
 
-  $('#newPasswordForm').on('submit', (event => {
-    event.preventDefault();
-
-    const data = {
-      service_name: $('#service_name').val(),
-      service_url: $('#service_url').val(),
-      login_username: $('#login_username').val(),
-      login_password: $('#login_password').val(),
-      description: $('#description').val()
-    };
-
-    postPassword(data)
-      .then((res) => {
-        console.log(res);
-      });
-
-
-  }));
-
-  const generatePassword = (length, hasCapitals, hasSpecialCharacters) => {
-
-    const specialChars = ['!', '@', '#', '$', '%', '&', '*'];
-    let password = '';
-    // Start with a guaranteed number - very small chance the rest of the script produces a password that is empty of numbers
-    password += Math.floor(Math.random() * 10);
-    // Create a random string at least 255 characters long
-    for (let i = 0; i < 25; i++) {
-      password += Math.random().toString(36).slice(2);
-    }
-    // Extract only the length the user wants
-    password = password.slice(0, length);
-    // If user wants capitals, toggle some non-number chars to uppercase
-    if (hasCapitals) {
-      let output = '';
-      for (let char of password) {
-        const toggle = Math.floor(Math.random() * 3);
-        if (isNaN(char) && toggle === 1) {
-          output += char.toUpperCase();
-          continue;
-        }
-        output += char;
-      }
-      password = output;
-    }
-    // If user wants special characters, toggle some non-number and non-capitalized chars to special characters
-    if (hasSpecialCharacters) {
-      let output = '';
-      for (let char of password) {
-        const toggle = Math.floor(Math.random() * 2);
-        if (isNaN(char) && toggle === 1 && char.toUpperCase() !== char) {
-          output += specialChars[Math.floor(Math.random() * 7)];
-          continue;
-        }
-        output += char;
-      }
-      password = output;
-    }
-
-    return password;
-
-  };
-
-  $('#toggleGenerate').on('click', (event => {
-    const generatePasswordFields = `
+  const $generatePasswordFields = $(`
       <form id="generatePasswordForm">
         <input id="generatePasswordLength" name="generatePasswordLength" type="number" placeholder="Length">
         <input id="generatePasswordIsUppercase" type="checkbox" name="generatePasswordIsUppercase" value="true">
