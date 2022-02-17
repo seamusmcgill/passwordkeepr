37757edd3c97239996abd061e0db200bf14418ac
Seamus McGill <seamusmcgill@gmail.com>
2022-02-16 20:29:58 -0500

    Add functionality to join or create an organization

--- public/scripts/components/registration_form.js
+++ public/scripts/components/registration_form.js
@@ -6,35 +6,64 @@ $(() => {
     </div>
     <div class="registration-fields">
       <input type="text" name="full_name" id="full_name" placeholder="Full Name">
-      <select name="organization" id="organization-select">
-      </select>
       <input type="email" name="email" id="email" placeholder="Email">
       <input type="password" name="password" id="password" placeholder="Password">
-      <button>Register</button>
     </div>
+    <div class="organization-options">
+      <button id="join-organization">Join Organization</button>
+      <button id="create-organization">Create Organization</button>
+    </div>
+    <button>Register</button>
   </form>
   `);
 
   window.$registrationForm = $registrationForm;
 
+  const $organizationSelect = $(`
+      <select name="organization" id="organization-select"></select>
+      `);
+
+  window.$organizationSelect = $organizationSelect;
+
+  const $createOrganizationForm = $(`
+      <div class="create-organization-fields">
+          <input type="text" name="organization_name" id="organization_name" placeholder="Organization Name">
+          <input type="password" name="organization_password" id="organization_password" placeholder="Organization Password">
+          <input type="text" name="organization_url" id="organization_url" value="https://">
+          <input type="text" name="organization_description" id="organization_description" placeholder="Description">
+          <input type="text" name="organization_logo_url" id="organization_logo_url" value="https://">
+      </div>
+      `);
+
   const createOrganizationsSelect = (organizations) => {
     const organizationsArray = organizations.organizations;
     for (const organization of organizationsArray) {
       let $organizationOption = $(`<option value='${organization.id}'>${organization.name}</option>`);
-      $('#organization-select').append($organizationOption);
+      $organizationSelect.append($organizationOption);
     }
+    console.log("ORGANIZATIONSELECT", $organizationSelect.html());
+    $organizationSelect.appendTo(".organization-options");
   };
 
-  $navBar.on('click', '#registerLink', (event) => {
+
+  // Generate organizations dropdown on button click
+  $(document).on('click', '#join-organization', (event) => {
     event.preventDefault();
-
-    viewsManager.show('registration');
-
     getOrganizations()
       .then((res) => {
+        $organizationSelect.empty();
         createOrganizationsSelect(res);
-        viewsManager.show('registration');
+        $('.organization-options button').hide();
       });
+  });
+
+  // Generate create organization form on button click
+  $(document).on('click', '#create-organization', (event) => {
+    event.preventDefault();
+
+    $createOrganizationForm.appendTo(".organization-options");
+    $('.organization-options button').hide();
+
   });
 
   $(document).on("submit", "#registration-form", event => {
@@ -47,15 +76,44 @@ $(() => {
       password: $('#password').val(),
     };
 
-    createUser(data)
-      .then(res => {
-        header.update(res.user);
-        getPasswords()
-          .then((response) => {
-            passwords.renderPasswords(response);
-            viewsManager.show('passwords');
-          });
-      });
+    // If no value in the organization select -> create organization before creating user
+    if (!$('#organization-select').val()) {
+      const organizationData = {
+        name: $('#organization_name').val(),
+        password: $('#organization_password').val(),
+        organization_url: $('#organization_url').val(),
+        description: $('#organization_description').val(),
+        organization_logo_url: $('#organization_logo_url').val(),
+      };
+
+      createOrganization(organizationData)
+        .then(res => {
+          data.organization_id = res.id;
+          createUser(data)
+            .then(res => {
+              header.update(res.user);
+              getPasswords()
+                .then((response) => {
+                  passwords.renderPasswords(response);
+                  viewsManager.show('passwords');
+                });
+            });
+        });
+    } else {
+      // NEED TO FIND A WAY SO THIS WHOLE BLOCK FROM LIKE 92 DOESN'T NEED TO REPEAT
+      createUser(data)
+        .then(res => {
+          header.update(res.user);
+          getPasswords()
+            .then((response) => {
+              passwords.renderPasswords(response);
+              viewsManager.show('passwords');
+            });
+        });
+    }
+
+
+
   });
 
 });
