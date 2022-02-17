22f4bf6151e6a5d17c361eef1b6af1971df57dbc
Seamus McGill <seamusmcgill@gmail.com>
2022-02-16 20:29:04 -0500

    Add post route to create organization

--- routes/organizations.js
+++ routes/organizations.js
@@ -21,5 +21,28 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.post("/", (req, res) => {
+    const newOrganizationObject = req.body;
+    const parameters = [`${newOrganizationObject.name}`, `${newOrganizationObject.password}`, `${newOrganizationObject.organization_url}`, `${newOrganizationObject.description}`, `${newOrganizationObject.logo_url}`];
+    db.query(`
+    INSERT INTO organizations
+      (name, password, organization_url, description, logo_url)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING
+      *;
+    `, parameters)
+      .then(data => {
+        const organization = data.rows[0];
+        res.json({ id: organization.id });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
