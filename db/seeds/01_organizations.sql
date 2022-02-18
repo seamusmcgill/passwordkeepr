dedcdebb4bc6b1c8c60fa65889214d988809ffe9
Andrew McNeill <andrew@andrewmc.ca>
2022-02-18 09:53:28 -0400

    Changes HTML title and updates db seeds

--- db/seeds/01_organizations.sql
+++ db/seeds/01_organizations.sql
@@ -1,4 +1,6 @@
 INSERT INTO organizations
   (name, password, organization_url, description, logo_url)
 VALUES
-  ('Lighthouse Labs', 'password', 'https://www.lighthouselabs.ca/en', 'Lighthouse Labs was created in 2013 by a team of software developers with a passion for code, mentorship, and education. Our mission: to continuously find the best ways to train the next generation of developers and transform the way tech education is delivered.', 'https://coursereport-production.imgix.net/uploads/school/logo/46/original/lighthouse-labs-logo-compressor.png?w=200&h=200');
+  ('Lighthouse Labs', 'password', 'https://www.lighthouselabs.ca/en', 'Lighthouse Labs was created in 2013 by a team of software developers with a passion for code, mentorship, and education. Our mission: to continuously find the best ways to train the next generation of developers and transform the way tech education is delivered.', 'https://coursereport-production.imgix.net/uploads/school/logo/46/original/lighthouse-labs-logo-compressor.png?w=200&h=200'),
+  ('freeCodeCamp', 'password', 'https://www.freecodecamp.org', 'Our mission: to help people learn to code for free. We accomplish this by creating thousands of videos, articles, and interactive coding lessons - all freely available to the public.', 'https://design-style-guide.freecodecamp.org/downloads/fcc_secondary_small.svg'),
+  ('Shopify', 'password', 'https://www.shopify.com', 'The platform commerce is built on', 'https://logos-world.net/wp-content/uploads/2020/11/Shopify-Emblem.png');
--- db/seeds/02_users.sql
+++ db/seeds/02_users.sql
@@ -3,5 +3,5 @@ INSERT INTO users
   (organization_id, full_name, email, password, admin)
 VALUES
   (1, 'Andrew McNeill', 'andrew@andrewmc.ca', 'password', TRUE),
-  (1, 'Seamus McGill', 'email@email.com', 'password', DEFAULT),
-  (1, 'Barento Badaso', 'email@email.com', 'password', DEFAULT);
+  (2, 'Seamus McGill', 'seamusmcgill@gmail.com', 'password', DEFAULT),
+  (3, 'Barento Badaso', 'barentobadaso@gmail.com', 'password', DEFAULT);
