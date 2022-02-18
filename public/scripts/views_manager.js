8be3543f0f8d5b30ab9d00953c5f467ca531d208
Seamus McGill <seamusmcgill@gmail.com>
2022-02-17 23:51:02 -0500

    Reset form on view reload

--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -13,23 +13,29 @@ $(() => {
       break;
     case 'newPassword':
       $newPasswordHTML.appendTo('main');
+      $('#newPasswordForm').trigger("reset");
       generateCategoryOptions();
       break;
     case 'editPassword':
       $editPasswordForm.appendTo('main');
+      $editPasswordForm.trigger('reset');
       generateCategoryOptions();
       break;
     case 'newCategory':
       $newCategoryHTML.appendTo('main');
+      $newCategoryHTML.trigger("reset");
       break;
     case 'editCategory':
       $editCategoryForm.appendTo('main');
+      $editCategoryForm.trigger('reset');
       break;
     case 'registration':
       $registrationForm.appendTo('main');
+      $editCategoryForm.trigger('reset');
       break;
     case 'login':
       $logInForm.appendTo('main');
+      $logInForm.trigger('reset');
       break;
     case 'error': {
       const $error = $(`<p>${arguments[1]}</p>`);
